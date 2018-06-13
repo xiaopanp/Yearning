8b5611c8eba433974e98ee907237ed26db55e2ef
xiaopanp <crq12@163.com>
2018-06-13 17:15:54 +0800

    Update docker_start.sh

--- install/docker_start.sh
+++ install/docker_start.sh
@@ -9,7 +9,7 @@ if [ ! -d "/var/lib/mysql/Yearning" ]; then
   mysql -uroot -p"$MYSQLPASSWORD" -e "create database Yearning character set 'utf8' collate 'utf8_general_ci' ;"
   python3 manage.py makemigrations
   python3 manage.py migrate
-  echo "from core.models import Account; Account.objects.create_user(username='admin', password='Yearning_admin', group='admin',is_staff=1)" | python3 manage.py shell
+  echo "from core.models import Account; Account.objects.create_user(username='admin', password='123456', group='admin',is_staff=1)" | python3 manage.py shell
 fi
 
 
