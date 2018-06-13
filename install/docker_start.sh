a2489f532d4d761db92d898728b8989ff99ccfc3
xiaopanp <crq12@163.com>
2018-06-13 11:29:16 +0800

    Update docker_start.sh

--- install/docker_start.sh
+++ install/docker_start.sh
@@ -3,14 +3,15 @@ chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
 /usr/bin/mysqld_safe &
 sleep 3
 
+mysql -uroot -e "grant all on *.* to root@localhost identified by '${MYSQLPASSWORD}'; flush privileges;"
+
 if [ ! -d "/var/lib/mysql/Yearning" ]; then
-  mysql -e "create database Yearning character set 'utf8' collate 'utf8_general_ci' ;"
+  mysql -uroot -p"$MYSQLPASSWORD" -e "create database Yearning character set 'utf8' collate 'utf8_general_ci' ;"
   python3 manage.py makemigrations
   python3 manage.py migrate
   echo "from core.models import Account; Account.objects.create_user(username='admin', password='Yearning_admin', group='admin',is_staff=1)" | python3 manage.py shell
 fi
 
-mysql -uroot -e "grant all on *.* to root@localhost identified by '${MYSQLPASSWORD}'; flush privileges;"
 
 sed -i "s/8000/${YPort}/" /usr/share/nginx/html/static/js/app.b534e9fe0e47062ceee8.js
 sed -i "56,56d"  /opt/Yearning/src/settingConf/settings.py
