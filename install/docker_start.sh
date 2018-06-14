476bfdcad273ff9762c6110b6682c8760049f214
xiaopanp <crq12@163.com>
2018-06-14 18:50:13 +0800

    temp

--- install/docker_start.sh
+++ install/docker_start.sh
@@ -6,6 +6,7 @@ mysqladmin shutdown & sleep 10
 cp -rf /var/lib/mysql/* /opt/mysql/data/
 cp -rf /opt/Yearning/my.cnf /etc/my.cnf
 mysql_install_db
+chown -R mysql:mysql /opt/mysql/data /var/run/mysqld
 /usr/bin/mysqld_safe & sleep 10
 mysql -uroot -e "grant all on *.* to root@localhost identified by 'root'; flush privileges;"
  
